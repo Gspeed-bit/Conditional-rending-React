25678d3dbd1d5c786acc6b7ad47836022d1b513a
Your Name <adeyeye17@gmail.com>
2021-09-20 14:01:53 +0200

    conditional rendering using componentDidMount

---
+++ src/Component/Conditional.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Conditional extends Component {
+    render() {
+        return(
+            <div>
+  <h1>Some cool stuff about conditional rendering</h1>
+        </div>
+        )
+    }
+}
